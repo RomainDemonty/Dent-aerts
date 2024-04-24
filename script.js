fcb222e630ad50a482ddb47080844ca3632562da
RomainDemonty <romain.demonty@student.hepl.be>
2024-04-24 14:05:16 +0200

    petit bug menu déroulant

---
+++ script.js
@@ -0,0 +1,44 @@
+function toggleMenu() {
+    var menuBtn = document.querySelector('.menu-btn');
+    var menuList = document.getElementById('menu-list');
+
+    // Basculer la classe menu-open sur le bouton
+    menuBtn.classList.toggle('menu-open');
+
+    // Basculer la classe menu-open sur la liste de menu
+    menuList.classList.toggle('menu-open');
+
+    // Afficher ou masquer la liste de menu en fonction de l'état du bouton
+    if (menuBtn.classList.contains('menu-open')) {
+        // Si la classe menu-open est présente, afficher la liste de menu
+        menuList.querySelectorAll('a').forEach(function(item) {
+            item.style.display = 'wrap';
+        });
+    } else {
+        // Sinon, cacher la liste de menu
+        menuList.querySelectorAll('a').forEach(function(item) {
+            item.style.display = 'none';
+        });
+    }
+}
+
+
+// Afficher le bouton de menu sur les appareils mobiles
+function adjustMenuDisplay() {
+    var menuBtn = document.querySelector('.menu-btn');
+    var menuList = document.querySelector('menu');
+
+    if (window.innerWidth <= 600) {
+        menuBtn.style.display = 'flex';
+        menuList.style.display = menuBtn.classList.contains('menu-open') ? 'flex' : 'none';
+    } else {
+        menuBtn.style.display = 'none';
+        menuList.style.display = 'flex';
+    }
+}
+
+// Mettre à jour l'affichage du menu lorsque la taille de la fenêtre change
+window.addEventListener('resize', adjustMenuDisplay);
+
+// Appeler la fonction pour ajuster l'affichage du menu lors du chargement de la page
+window.addEventListener('DOMContentLoaded', adjustMenuDisplay);
